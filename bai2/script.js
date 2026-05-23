7c58b39536d338015d4d1c34ed4b5f197eb4748a
Le Yen Vi <leyenvi1046@gmai.com>
2026-05-23 15:31:15 +0700

    giai đoạn 3: Xử lý sự kiện cơ bản

--- bai2/script.js
+++ bai2/script.js
@@ -53,3 +53,133 @@ function showMessage(message) {
     messageBox.innerText = message;
 
 }
+
+// =======================
+// INPUTS
+// =======================
+
+const titleInput = document.getElementById("title");
+
+const descriptionInput =
+    document.getElementById("description");
+
+const deadlineInput =
+    document.getElementById("deadline");
+
+const priorityInput =
+    document.getElementById("priority");
+
+const taskForm =
+    document.getElementById("taskForm");
+
+const taskList =
+    document.getElementById("taskList");
+
+
+// =======================
+// DATA
+// =======================
+
+let tasks = [];
+
+
+// =======================
+// SUBMIT FORM
+// =======================
+
+taskForm.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+
+    const task = {
+
+        title: titleInput.value,
+
+        description: descriptionInput.value,
+
+        deadline: deadlineInput.value,
+
+        priority: priorityInput.value,
+
+        completed: false
+
+    };
+
+
+    tasks.push(task);
+
+
+    renderTasks();
+
+    resetForm();
+
+    taskModal.classList.add("hidden");
+
+    showMessage("Thêm công việc thành công");
+
+});
+
+
+// =======================
+// RENDER TASKS
+// =======================
+
+function renderTasks() {
+
+    if (tasks.length === 0) {
+
+        taskList.innerHTML = `
+            <p>Chưa có công việc nào</p>
+        `;
+
+        return;
+    }
+
+
+    let html = "";
+
+
+    tasks.forEach(function (task) {
+
+        html += `
+            <div class="task-card">
+
+                <h3>${task.title}</h3>
+
+                <p>${task.description}</p>
+
+                <p>Deadline: ${task.deadline}</p>
+
+                <p>Ưu tiên: ${task.priority}</p>
+
+                <p>
+                    Trạng thái:
+                    ${task.completed
+                        ? "Đã hoàn thành"
+                        : "Chưa hoàn thành"}
+                </p>
+
+                <button>Sửa</button>
+
+                <button>Xóa</button>
+
+            </div>
+        `;
+    });
+
+
+    taskList.innerHTML = html;
+
+}
+
+
+// =======================
+// RESET FORM
+// =======================
+
+function resetForm() {
+
+    taskForm.reset();
+
+}
